d1d606eea4ff3e9bab0a7c8dad147d7451cf3f24
victorascencioh <victorascencioh@gmail.com>
2018-11-02 10:18:19 +0100

    Updates

---
+++ precache-manifest.704b09c401ec84378abc18812aa02e6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7f96ab5e68aead5e091",
+    "url": "/cotizador/static/js/runtime~main.e7f96ab5.js"
+  },
+  {
+    "revision": "ee4c2c73e77e81f28ee5",
+    "url": "/cotizador/static/js/main.ee4c2c73.chunk.js"
+  },
+  {
+    "revision": "36e6af7bbf520bce8013",
+    "url": "/cotizador/static/js/1.36e6af7b.chunk.js"
+  },
+  {
+    "revision": "ee4c2c73e77e81f28ee5",
+    "url": "/cotizador/static/css/main.4a8ee012.chunk.css"
+  },
+  {
+    "revision": "a938bf0b15002c7ab39b155cf0b68670",
+    "url": "/cotizador/index.html"
+  }
+];
